c7c2bc2b103642797dd875c2259ef314cbecc973
Angelo Mab <g.angelo.m@gmail.com>
2021-11-11 09:42:11 -0500

    project done

--- 05-JWT-Basics/starter/app.js
+++ 05-JWT-Basics/starter/app.js
@@ -4,17 +4,20 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const mainRouter = require('./routes/main')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
-app.use(express.static('./public'));
-app.use(express.json());
+app.use(express.static('./public')); // static files where HTML lives
+app.use(express.json()); // one of the routes will be a POST route and we need req.body
 
+
+app.use('/api/v1', mainRouter)  // make sure this is after the middleware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const start = async () => {
   try {
@@ -27,3 +30,8 @@ const start = async () => {
 };
 
 start();
+
+
+// JWT is a way to exchange data between 2 parties
+// ie. FE to API
+// better: security feature
